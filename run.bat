ee0422bf3c2b0f272e0e1bfd83ac94890a94d79e
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2025-02-19 17:15:12 +0200

    Update run batch file

--- run.bat
+++ run.bat
@@ -1,3 +1,6 @@
+set JAVA_HOME=%JAVA_HOME_17%
+
 call mvn clean install -DskipTests
 
-call java -agentpath:%USER_HOME%/agent/lightrun_agent.dll -jar target/spring-account-transfer-1.0.0-SNAPSHOT.jar
+rem call java -Duser.timezone="UTC" -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5005 -jar target/spring-account-transfer-1.0.0-SNAPSHOT.jar
+call java -Duser.timezone="UTC" -jar target/spring-account-transfer-1.0.0-SNAPSHOT.jar
